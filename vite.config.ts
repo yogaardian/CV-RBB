df9c0876de11bdfa3795b01dc2038fe66ea3adbf
sibroenih <sibro3964@gmail.com>
2026-07-27 14:40:31 +0700

    nitro install redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
@@ -9,6 +10,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    nitro(),
     tailwindcss(),
     react(),
   ],
